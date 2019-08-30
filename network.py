847d933b51ba90480f441af5f1459c6b11478428
Heewoong Lee <37352488+PlanNoa@users.noreply.github.com>
2019-08-30 16:52:47 +0900

    change loss functions to mse

--- network.py
+++ network.py
@@ -1,17 +1,22 @@
 from keras import layers
 from keras.models import Model
-from keras.losses import mean_squared_error, kullback_leibler_divergence, binary_crossentropy
+from keras.losses import mean_squared_error
 import numpy as np
 
 class Network:
 
     def __init__(self, NUM_Category):
 
-        self.inputs = layers.Input(shape=(3,), name='input')
+        self.inputs = layers.Input(shape=(5,), name='input')
 
         x = layers.Dense(16, activation='relu')(self.inputs)
+        x = layers.Dropout(0.50)(x)
         x = layers.Dense(20, activation='relu')(x)
-        x = layers.Dropout(0.25)(x)
+        x = layers.Dropout(0.50)(x)
+        x = layers.Dense(20, activation='sigmoid')(x)
+        x = layers.Dropout(0.50)(x)
+        x = layers.Dense(20, activation='sigmoid')(x)
+        x = layers.Dropout(0.50)(x)
         a_prediction = layers.Dense(NUM_Category, activation='softmax', name='a')(x)
         b_prediction = layers.Dense(NUM_Category, activation='softmax', name='b')(x)
         c_prediction = layers.Dense(NUM_Category, activation='softmax', name='c')(x)
@@ -19,7 +24,7 @@ class Network:
         self.model = Model(self.inputs, [a_prediction, b_prediction, c_prediction])
 
         self.model.compile(optimizer='rmsprop',
-                      loss={'a': mean_squared_error, 'b': kullback_leibler_divergence, 'c': binary_crossentropy},
+                      loss={'a': mean_squared_error, 'b': mean_squared_error, 'c': mean_squared_error},
                       loss_weights={'a': 0.25, 'b': 1., 'c': 10.})
 
     def fit(self, train, a_targets, b_targets, c_targets):
