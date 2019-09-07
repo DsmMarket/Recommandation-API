9322393b128d5ee78d20090e8eae09ae29168662
Heewoong Lee <37352488+PlanNoa@users.noreply.github.com>
2019-09-07 19:00:52 +0900

    change load_model function

--- network.py
+++ network.py
@@ -1,19 +1,21 @@
 from keras import layers
-from keras.models import Model, load_model
+from keras.models import Model,  model_from_json
 from keras.losses import mean_squared_error
 import numpy as np
 import os
+import json
 
 class Network:
 
-    def __init__(self, NUM_Category, model_path = 'model.h5'):
+    def __init__(self, NUM_Category, model_path = 'model.h5', weight_path = 'model_in_json.json'):
 
-        if os.path.exists(model_path):
-            self.model = load_model(model_path)
+        self.model = ''
 
+        if os.path.exists(model_path) and os.path.exists(weight_path):
+            self.load_model(model_path, weight_path)
         else:
             self.inputs = layers.Input(shape=(5,), name='input')
-            x = layers.Dense(16, activation='relu', input_shape=(5, ))(self.inputs)
+            x = layers.Dense(16, activation='relu', input_dim=5)(self.inputs)
             x = layers.Dropout(0.50)(x)
             x = layers.Dense(20, activation='relu')(x)
             x = layers.Dropout(0.50)(x)
@@ -26,7 +28,8 @@ class Network:
             c_prediction = layers.Dense(NUM_Category, activation='softmax', name='c')(x)
 
             self.model = Model(self.inputs, [a_prediction, b_prediction, c_prediction])
-            self.model.compile(optimizer='rmsprop',
+
+        self.model.compile(optimizer='rmsprop',
                           loss={'a': mean_squared_error, 'b': mean_squared_error, 'c': mean_squared_error},
                           loss_weights={'a': 1., 'b': 1., 'c': 1.})
 
@@ -42,11 +45,17 @@ class Network:
         return [self.pred_a, self.pred_b, self.pred_c]
 
     def save_model(self, model_path):
+        model_json = self.model.to_json()
         if model_path is not None and self.model is not None:
-            self.model.save_weights(model_path, overwrite=True)
-        else:
+            with open("model_in_json.json", 'w') as json_file:
+                json.dump(model_json, json_file)
+
             self.model.save_weights(model_path)
 
-    # def load_model(self, model_path):
-    #     if model_path is not None and os.path.exists(model_path):
-    #         self.model.load_weights(model_path)
+    def load_model(self, model_path, weight_path):
+        if model_path is not None and os.path.exists(model_path):
+            with open(weight_path, 'r') as f:
+                self.model = model_from_json(json.load(f))
+            try: self.model.load_weights(model_path)
+            except: pass
+            print('model load complete')
