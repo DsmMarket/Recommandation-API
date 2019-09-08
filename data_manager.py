074509aec8f72f97856b85411c7b3d6cf9d1fad8
Heewoong Lee <37352488+PlanNoa@users.noreply.github.com>
2019-09-08 14:33:07 +0900

    make all 'res' to meaningful name

--- data_manager.py
+++ data_manager.py
@@ -5,17 +5,18 @@ class DataManager:
     def __init__(self, NUM_Category = 0):
         self. NUM_Category = NUM_Category
 
-    # example data = [[1, 14, 12, 7, 6, 5, 1, 2]]
-    # 1, 14, 12번 카테고리 상품을 보고 난 뒤, 7, 6번 상품을 보고, 5번 상품을 보고 있는 경우.
-    # 1은 남성을 의미, 2는 2학년을 의미.
-    def build_learndata(self, data, X = [], a = [], b = [], c = []):
+    def build_learndata(self, data):
         if data is not None:
             data = pd.DataFrame(np.array(data))
-            X = pd.concat([pd.DataFrame(X),
-                           pd.concat([data[0], data[1], data[2], data[6], data[7]], axis=1)], axis=0)
-            a = pd.concat([pd.DataFrame(a), data[3]], axis=0)
-            b = pd.concat([pd.DataFrame(b), data[4]], axis=0)
-            c = pd.concat([pd.DataFrame(c), data[5]], axis=0)
+            X = pd.concat([pd.DataFrame([]),
+                           pd.concat([data[0],
+                                      data[1],
+                                      data[2],
+                                      data[6],
+                                      data[7]], axis=1)], axis=0)
+            a = pd.concat([pd.DataFrame([]), data[3]], axis=0)
+            b = pd.concat([pd.DataFrame([]), data[4]], axis=0)
+            c = pd.concat([pd.DataFrame([]), data[5]], axis=0)
 
             X = X.values.tolist()
             a_target = self.build_ytrain(a.values.tolist())
@@ -26,30 +27,30 @@ class DataManager:
         return False
 
     def toint(self, array):
-        res = []
+        intarray = []
         for obj in array:
-            res.append(list(map(int, obj)))
-        return res
+            intarray.append(list(map(int, obj)))
+        return intarray
 
     def build_ytrain(self, ytrain):
-        res = []
+        train_data = []
         for d in ytrain:
             temp = [0] * self.NUM_Category
             temp[int(d[0])] = 1
-            res.append(temp)
-        return res
+            train_data.append(temp)
+        return train_data
 
     def build_preddata(self, data):
-        if data is not None:
-            data = pd.DataFrame(np.array(data))
-            data = pd.concat([pd.DataFrame([]),
-                           pd.concat([data[3], data[4], data[5], data[6], data[7]], axis=1)], axis=0)
-            return data
-        return False
+        data = pd.DataFrame(np.array(data))
+        data = pd.concat([pd.DataFrame([]),
+                          pd.concat([data[3],
+                                     data[4],
+                                     data[5],
+                                     data[6],
+                                     data[7]], axis=1)], axis=0)
+        return data
 
     def merge_data(self, data):
-        if data is not None or len(data[0]) != 8:
-            train_data = self.build_learndata(data)
-            pred_data = self.build_preddata(data)
-            return train_data, pred_data
-        return False
+        train_data = self.build_learndata(data)
+        pred_data = self.build_preddata(data)
+        return train_data, pred_data
